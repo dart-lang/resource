f8f25f96a2d1d1b18e4b1260727821d626c5fff2
rbellens <rik.bellens@telin.ugent.be>
2018-11-03 22:11:10 +0100

    donot use isolate on web platform

--- lib/src/isolate_html.dart
+++ lib/src/isolate_html.dart
@@ -2,6 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-export "isolate_none.dart"
-    if (dart.library.html) "isolate_html.dart"
-    if (dart.library.isolate) "isolate_isolate.dart";
+import 'dart:async';
+
+Future<Uri> resolveUri(Uri uri) {
+  if (uri.scheme == "package") {
+    uri = Uri.parse("packages/${uri.path}");
+  }
+  return new Future<Uri>.value(Uri.base.resolveUri(uri));
+}
--- lib/src/isolate_none.dart
+++ lib/src/isolate_none.dart
@@ -2,6 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-export "isolate_none.dart"
-    if (dart.library.html) "isolate_html.dart"
-    if (dart.library.isolate) "isolate_isolate.dart";
+import 'dart:async';
+
+Future<Uri> resolveUri(Uri uri) {
+  if (uri.scheme == "package") {
+    throw new UnsupportedError("Unsupported scheme: $uri");
+  }
+  return new Future<Uri>.value(Uri.base.resolveUri(uri));
+}
